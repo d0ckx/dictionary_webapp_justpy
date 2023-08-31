023bb7d6e538cb83ce945b4d757398005d546d06
pieter <dockx@pm.me>
2023-08-31 17:28:55 +0200

    implement get_definition static method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import justpy as jp
 
 from webapp.about_page import About
+from webapp.dictionary_page import Dictionary
 from webapp.homepage import Home
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy()
